a3205e4fc870e34da039ab30c6ec393170e2c3f7
samang22 <samang22@naver.com>
2018-11-16 15:43:56 +0900

    done
    done

--- term/TheBindingOfIssac/play_state.py
+++ term/TheBindingOfIssac/play_state.py
@@ -2,10 +2,11 @@ from pico2d import *
 import issac 
 import fly
 import meat
+import hopper
 import UI
 
 def handle_events():
-    global play_issac
+    global play_issac, meatlist, hopperlist
     events = get_events()
     for e in events:
         if e.type == SDL_QUIT:
@@ -17,6 +18,10 @@ def handle_events():
             if len(meatlist) > 0:
                 for m in meatlist:
                     m.Hit()
+        elif e.type == SDL_KEYDOWN and e.key == SDLK_h:
+            if len(hopperlist) > 0:
+                for h in hopperlist:
+                    h.Hit()
 
         else:
             play_issac.handle_event(e)
@@ -25,7 +30,7 @@ def handle_events():
             
 
 def enter():
-    global play_issac, play_UI, flylist, meatlist
+    global play_issac, play_UI, flylist, meatlist, hopperlist
     open_canvas()
     play_issac = issac.Issac()
     play_UI = UI.UI()
@@ -36,10 +41,13 @@ def enter():
     meatlist = []
     meatlist.append(meat.Meat())
 
+    hopperlist = []
+    hopperlist.append(hopper.Hopper())
+
     play_UI.SetData(play_issac.GetBombNum(), play_issac.GetLifeNum(),play_issac.GetKeyNum(), play_issac.GetArrowKind())
 
 def draw():
-    global play_issac, play_UI, flylist, meatlist
+    global play_issac, play_UI, flylist, meatlist, hopperlist
     clear_canvas()
     play_UI.draw()
     play_issac.draw()
@@ -49,6 +57,9 @@ def draw():
     if len(meatlist) > 0:
        for m in meatlist:
          m.draw()
+    if len(hopperlist) > 0:
+       for h in hopperlist:
+         h.draw()
     update_canvas()
 
 def update():
@@ -62,6 +73,9 @@ def update():
        for m in meatlist:
          m.update()
          m.SetIssacPos(play_issac.GetX(), play_issac.GetY())
+    if len(hopperlist) > 0:
+       for h in hopperlist:
+         h.update()
     
     delay(0.03)
 
